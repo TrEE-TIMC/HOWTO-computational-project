74e985a6ed4fb2d8554332c3dd8ccdcfa97505ae
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-03-18 12:40:13 +0100

    Adding travis compilation

---
+++ .build_tools/travis/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This script is meant to be called by the "script" step defined in
+# .travis.yml. See http://docs.travis-ci.com/ for more details.
+# The behavior of the script is controlled by environment variabled defined
+# in the .travis.yml in the top level folder of the project.
+
+# License: 3-clause BSD
+
+set -e
+
+run_tests() {
+    pushd docs/
+    make html SPHINXOPTS="-W"
+    popd
+}
+
+
+run_tests
